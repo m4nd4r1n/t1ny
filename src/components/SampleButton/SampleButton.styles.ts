48177b91d443c5bd08cbd5d381155c14ce2465fc
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-09-14 22:49:28 +0900

    fix: sample button

--- src/components/SampleButton/SampleButton.styles.ts
+++ src/components/SampleButton/SampleButton.styles.ts
@@ -1,43 +1,17 @@
-import { styled } from '@/styled-system/jsx';
+import { tv } from 'tailwind-variants';
 
-export const Button = styled('button', {
-  base: {
-    display: 'inline-block',
-    cursor: 'pointer',
-    borderRadius: '3em',
-    borderWidth: '0px',
-    fontFamily: 'sans',
-    fontWeight: 'bold',
-    lineHeight: '1',
-  },
+export const sampleButton = tv({
+  base: 'inline-block cursor-pointer rounded-[3em] border-0 font-sans font-bold leading-none',
   variants: {
     primary: {
-      true: {
-        backgroundColor: '#1ea7fd',
-        color: 'white',
-      },
-      false: {
-        backgroundColor: 'transparent',
-        color: '#333',
-        boxShadow: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
-      },
+      true: 'bg-[#1ea7fd] text-white',
+      false:
+        'bg-transparent text-[#333] shadow-[rgba(0,0,0,0.15)_0px_0px_0px_1px_inset]',
     },
     size: {
-      small: {
-        fontSize: '12px',
-        px: '4',
-        py: '2.5',
-      },
-      medium: {
-        fontSize: '14px',
-        px: '5',
-        py: '2.5',
-      },
-      large: {
-        fontSize: '16px',
-        px: '6',
-        py: '3',
-      },
+      small: 'px-4 py-2.5 text-xs',
+      medium: 'px-5 py-2.5 text-sm',
+      large: 'px-6 py-3 text-base',
     },
   },
 });
