d1777c260d4fe996a12a3a4e6c52dd6be51e02cb
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-09-18 03:50:13 +0900

    test: link component

---
+++ src/components/Link/Link.test.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { Link } from '.';
+
+describe('Link component', () => {
+  it('should render correctly', () => {
+    const wrapper = render(<Link href='#' />);
+
+    expect(() => wrapper.unmount()).not.toThrow();
+  });
+
+  it('should no errors when href missing', () => {
+    const wrapper = render(<Link>Link</Link>);
+
+    expect(() => wrapper.unmount()).not.toThrow();
+  });
+
+  it('should have target="_blank" and rel="noopener noreferrer" when "isExternal" is true', () => {
+    const { container } = render(
+      <Link isExternal href='#'>
+        Link
+      </Link>,
+    );
+
+    expect(container.querySelector('a')?.rel).toBe('noopener noreferrer');
+    expect(container.querySelector('a')?.target).toBe('_blank');
+  });
+});
