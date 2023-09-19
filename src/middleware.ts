b74fa8ab362718958689f81b313d48b8b3e4950e
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-09-19 17:00:22 +0900

    feat: add token validation to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,41 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+import { getToken } from 'next-auth/jwt';
+
 export const config = {
   matcher: ['/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)'],
 };
 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl;
-  const hostname = request.headers
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const rootDomain = `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+  const hostname = req.headers
     .get('host')
-    ?.replace('localhost:3000', `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+    ?.replace('localhost:3000', rootDomain);
   const path = url.pathname;
 
-  if (hostname === `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+  if (hostname === rootDomain) {
     return NextResponse.rewrite(
-      new URL(`/home${path === '/' ? '' : path}`, request.url),
+      new URL(`/home${path === '/' ? '' : path}`, req.url),
     );
   }
 
-  if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+  if (hostname === `app.${rootDomain}`) {
+    const session = await getToken({ req });
+    if (!session && path !== '/login') {
+      return NextResponse.redirect(new URL(`/login`, req.url));
+    } else if (session && path === '/login') {
+      return NextResponse.redirect(new URL(`/`, req.url));
+    }
     return NextResponse.rewrite(
-      new URL(`/app${path === '/' ? '' : path}`, request.url),
+      new URL(`/app${path === '/' ? '' : path}`, req.url),
     );
   }
 
-  if (hostname === `admin.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+  if (hostname === `admin.${rootDomain}`) {
     return NextResponse.rewrite(
-      new URL(`/admin${path === '/' ? '' : path}`, request.url),
+      new URL(`/admin${path === '/' ? '' : path}`, req.url),
     );
   }
 
