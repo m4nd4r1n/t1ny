a2a12082152d350e267b891e4bd1d6e35d40d9a7
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-09-23 23:35:50 +0900

    style: modal storybook

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Modal, type ModalProps } from '.';
+import { Button } from '../Button';
+import { Input } from '../Input';
+import { modal } from './Modal.styles';
+
+const meta = {
+  title: 'Components/Modal',
+  component: Modal,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    onClose: {
+      table: {
+        disable: true,
+      },
+    },
+    isOpen: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className='flex max-w-2xl justify-center'>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const defaultProps = {
+  ...modal.defaultVariants,
+};
+
+const content = (
+  <div className='space-y-4'>
+    <h2 className='text-2xl'>Create a new link</h2>
+    <div>
+      <Input
+        type='text'
+        name='destination'
+        id='destination'
+        label='Destination'
+        placeholder='https://example.com/long-url'
+        isRequired
+      />
+    </div>
+    <Button type='submit' fullWidth>
+      Create
+    </Button>
+  </div>
+);
+
+const Template = ({ size }: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={open}>Open modal</Button>
+      <Modal isOpen={isOpen} onClose={close} size={size}>
+        {content}
+      </Modal>
+    </>
+  );
+};
+
+export const Default: Story = {
+  render: Template,
+  args: {
+    ...defaultProps,
+  },
+};
