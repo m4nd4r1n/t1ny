7cb3a7dddad6a3a714f46929a9385ca3d2e479e3
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-11 02:13:29 +0900

    feat(api): link deletion route

---
+++ src/app/api/link/[id]/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+
+import { BadRequestError } from '@/libs/error';
+import { withErrorHandler } from '@/libs/handler';
+import { withAuth } from '@/libs/lucia';
+import prisma from '@/libs/prisma';
+
+type Params = {
+  id: string;
+};
+
+export const DELETE = withErrorHandler(
+  withAuth<Params>(async (request, { params, session }) => {
+    const { id } = params;
+    const { userId } = session.user;
+
+    if (!id) throw new BadRequestError('No id provided');
+
+    await prisma.$transaction([
+      prisma.url.deleteMany({
+        where: { id, user: { id: userId } },
+      }),
+      prisma.user.update({
+        where: { id: userId },
+        data: {
+          total_limit: { increment: 1 },
+        },
+      }),
+    ]);
+
+    return NextResponse.json({ message: 'Deleted' });
+  }),
+);
