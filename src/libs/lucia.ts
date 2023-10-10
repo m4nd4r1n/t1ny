83b19659be305263b1a63451383d4f28b228b272
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-11 01:49:31 +0900

    feat: auth route wrapper

--- src/libs/lucia.ts
+++ src/libs/lucia.ts
@@ -1,11 +1,14 @@
 import * as context from 'next/headers';
+import type { NextRequest } from 'next/server';
 import { cache } from 'react';
 
 import { prisma } from '@lucia-auth/adapter-prisma';
 import { github, google } from '@lucia-auth/oauth/providers';
-import { lucia } from 'lucia';
+import { type AuthRequest, lucia } from 'lucia';
 import { nextjs_future } from 'lucia/middleware';
 import 'lucia/polyfill/node';
+
+import { ForbiddenError, UnauthorizedError } from '@/libs/error';
 
 import client from './prisma';
 
@@ -52,5 +55,30 @@ export const getPageSession = cache(() => {
   const authRequest = auth.handleRequest('GET', context);
   return authRequest.validate();
 });
+
+type WithAuthRouteHandler<T> = (
+  request: NextRequest,
+  context: {
+    params: T;
+    authRequest: AuthRequest;
+    session: Awaited<ReturnType<AuthRequest['validate']>>;
+  },
+) => Promise<Response>;
+
+export const withAuth =
+  <T>(handler: WithAuthRouteHandler<T>) =>
+  async (request: NextRequest, { params }: { params: T }) => {
+    const authRequest = auth.handleRequest(request.method, context);
+    const session = await authRequest.validate();
+
+    if (!session) {
+      throw new UnauthorizedError();
+    }
+    if (session.user.role === 'BLOCKED') {
+      throw new ForbiddenError();
+    }
+
+    return handler(request, { params, authRequest, session });
+  };
 
 export type Auth = typeof auth;
