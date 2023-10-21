d22f85a89448829f76d395434af7b5396603237c
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-22 00:50:35 +0900

    style: bar list storybook

--- src/components/BarList/mock.tsx
+++ src/components/BarList/mock.tsx
@@ -1,6 +1,16 @@
+import { FaGithub, FaReddit, FaXTwitter, FaYoutube } from 'react-icons/fa6';
+import { FcGoogle } from 'react-icons/fc';
+
 import { random } from '@/libs/utils';
 
 const names = ['X', 'Google', 'GitHub', 'Reddit', 'Youtube'];
+const icons = [
+  <FaXTwitter key='x' className='h-5 w-5 fill-default' />,
+  <FcGoogle key='google' className='h-5 w-5' />,
+  <FaGithub key='github' className='h-5 w-5 fill-default' />,
+  <FaReddit key='reddit' className='h-5 w-5 fill-orange' />,
+  <FaYoutube key='youtube' className='h-5 w-5 fill-red' />,
+];
 
 const base = names.map((name) => ({
   name,
@@ -8,3 +18,10 @@ const base = names.map((name) => ({
 }));
 
 export const mockData = base.sort((a, b) => b.value - a.value);
+
+export const mockDataWithIcon = base
+  .map((item, index) => ({
+    ...item,
+    icon: () => icons[index],
+  }))
+  .sort((a, b) => b.value - a.value);
