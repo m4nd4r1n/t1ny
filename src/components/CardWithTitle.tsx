2511f891d93e1260541bff9f6aa652f16d9d1006
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-23 00:44:05 +0900

    feat: card with title component

---
+++ src/components/CardWithTitle.tsx
@@ -0,0 +1,21 @@
+import Card from './Card';
+
+interface CardWithTitleProps extends React.PropsWithChildren {
+  className?: string;
+  title: string;
+}
+
+const CardWithTitle: React.FC<CardWithTitleProps> = ({
+  className,
+  children,
+  title,
+}) => {
+  return (
+    <Card className={className}>
+      <h2 className='mb-8 text-xl font-bold'>{title}</h2>
+      {children}
+    </Card>
+  );
+};
+
+export default CardWithTitle;
