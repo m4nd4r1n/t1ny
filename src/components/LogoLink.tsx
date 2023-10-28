273b7a7847ed22567b901bc0f96890919c64e13a
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-29 01:40:22 +0900

    refactor: separate dashboard header

--- src/components/LogoLink.tsx
+++ src/components/LogoLink.tsx
@@ -1,8 +1,11 @@
 import { Link } from './Link';
 
-const LogoLink = () => {
+interface LogoLinkProps {
+  className?: string;
+}
+const LogoLink: React.FC<LogoLinkProps> = ({ className }) => {
   return (
-    <Link href='/' color='default' isBlock>
+    <Link href='/' color='default' isBlock className={className}>
       <span className='font-icon text-4xl antialiased after:h-6 after:w-6 after:content-logo'></span>
     </Link>
   );
