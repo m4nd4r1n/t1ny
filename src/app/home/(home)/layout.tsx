a1ac8e20d95ef100afe1fb9169c0da7a58d8ac45
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-29 01:36:51 +0900

    feat: home page

--- src/app/home/(home)/layout.tsx
+++ src/app/home/(home)/layout.tsx
@@ -1,39 +1,17 @@
 import type { Metadata } from 'next';
 
-import { Link } from '@/components/Link';
-import {
-  Navbar,
-  NavbarContent,
-  NavbarItem,
-  NavbarLogo,
-} from '@/components/Navbar';
-import { httpScheme } from '@/libs/constants';
+import Footer from './Footer';
+import Header from './Header';
 
 const HomeLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div>
-      <Navbar>
-        <NavbarContent>
-          <NavbarLogo>
-            <Link href='/' color='default' isBlock>
-              <span className='font-icon text-4xl antialiased  after:h-6 after:w-6 after:content-logo'></span>
-              <span className='ml-2 text-2xl font-medium'>t1ny</span>
-            </Link>
-          </NavbarLogo>
-        </NavbarContent>
-        <NavbarContent justify='end'>
-          <NavbarItem>
-            <Link
-              href={`${httpScheme}://app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}
-              color='default'
-            >
-              Go to dashboard
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
-      <main className='h-screen-header max-w-nav p-8'>{children}</main>
-    </div>
+    <>
+      <Header />
+      <main className='mx-auto h-full w-full max-w-screen-lg p-8'>
+        {children}
+      </main>
+      <Footer />
+    </>
   );
 };
 
