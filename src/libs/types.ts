ac583c2777ad48efacefbbdcdefd7a55b405c47d
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-30 23:17:26 +0900

    fix: link type

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -11,9 +11,9 @@ export type PropsWithoutChildren<T = HTMLElement> = Omit<Props<T>, 'children'>;
 export type Link = {
   id: string;
   target_url: string;
-  created_at: string;
+  created_at: string | Date;
   target_favicon: string;
-  target_title?: string;
+  target_title?: string | null;
 };
 
 export type Links = Link[];
