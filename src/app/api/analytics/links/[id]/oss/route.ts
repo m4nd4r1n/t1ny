cd44d54876fdfcbae990245956ec996925b9202c
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-30 23:12:17 +0900

    feat(api): os analytics route

---
+++ src/app/api/analytics/links/[id]/oss/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server';
+
+import { withErrorHandler } from '@/libs/handler';
+import { withAuth } from '@/libs/lucia';
+import prisma from '@/libs/prisma';
+
+type Params = {
+  id: string;
+};
+
+export const GET = withErrorHandler(
+  withAuth<Params>(async (request, { session, params }) => {
+    const { userId } = session.user;
+    const { id } = params;
+
+    const result = await prisma.analytics.groupBy({
+      take: 5,
+      by: ['os'],
+      _count: {
+        _all: true,
+      },
+      where: {
+        url_id: id,
+        url: {
+          user_id: userId,
+        },
+      },
+      orderBy: {
+        _count: {
+          id: 'desc',
+        },
+      },
+    });
+
+    const formattedResult = result.map(({ os, _count }) => ({
+      value: _count._all,
+      name: os ?? 'unknown',
+    }));
+
+    return NextResponse.json(formattedResult);
+  }),
+);
