471074b973dd7a8159646d711a4c5a826d41f9ff
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-11-21 21:37:14 +0900

    refactor: divide chart tooltip wrapper, row

--- src/components/ChartTooltip/ChartTooltip.tsx
+++ src/components/ChartTooltip/ChartTooltip.tsx
@@ -14,6 +14,8 @@ interface ChartTooltipProps<TValue extends ValueType, TName extends NameType>
   valueFormatter: ValueFormatter;
 }
 
+const slots = chartTooltip();
+
 const ChartTooltip = <TValue extends ValueType, TName extends NameType>({
   active,
   label,
@@ -23,36 +25,53 @@ const ChartTooltip = <TValue extends ValueType, TName extends NameType>({
 }: ChartTooltipProps<TValue, TName>) => {
   if (!active || !payload) return null;
 
-  const slots = chartTooltip();
-
   return (
-    <div className={slots.wrapper()}>
+    <ChartTooltipWrapper>
       <div className={slots.labelWrapper()}>
         <p className={slots.label()}>{label}</p>
       </div>
       <div className={slots.rowBox()}>
         {payload.map(({ value, name }, index) => (
-          <div key={`id-${index}`} className={slots.rowWrapper()}>
-            <div className={slots.rowNameWrapper()}>
-              {name && (
-                <span
-                  className={slots.rowNameIcon({
-                    className: `bg-${categoryColorMap.get(`${name}`)}`,
-                  })}
-                />
-              )}
-              <p className={slots.rowName()}>{name}</p>
-            </div>
-            {value && (
-              <p className={slots.rowValue()}>
-                {valueFormatter(Number(value))}
-              </p>
-            )}
-          </div>
+          <ChartTooltipRow
+            key={`id-${index}`}
+            value={valueFormatter(Number(value))}
+            name={`${name}`}
+            color={categoryColorMap.get(`${name}`) ?? ''}
+          />
         ))}
       </div>
-    </div>
+    </ChartTooltipWrapper>
   );
 };
+
+interface ChartTooltipRowProps {
+  name: string;
+  value: string;
+  color: string;
+}
+
+export const ChartTooltipRow: React.FC<ChartTooltipRowProps> = ({
+  name,
+  value,
+  color,
+}) => (
+  <div className={slots.rowWrapper()}>
+    <div className={slots.rowNameWrapper()}>
+      {name && (
+        <span
+          className={slots.rowNameIcon({
+            className: `bg-${color}`,
+          })}
+        />
+      )}
+      <p className={slots.rowName()}>{name}</p>
+    </div>
+    <p className={slots.rowValue()}>{value}</p>
+  </div>
+);
+
+export const ChartTooltipWrapper: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => <div className={slots.wrapper()}>{children}</div>;
 
 export default ChartTooltip;
