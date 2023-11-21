38b04195de4a25dc533b0cc390539610706bee03
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-11-21 21:39:54 +0900

    test: donut chart

---
+++ src/components/DonutChart/DonutChart.test.tsx
@@ -0,0 +1,52 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+import { DonutChart } from '.';
+import { mockData } from './mock';
+
+global.ResizeObserver = ResizeObserver;
+
+const category = 'sales';
+const index = 'name';
+
+describe('DonutChart component', () => {
+  beforeAll(() => {
+    jest
+      .spyOn(HTMLElement.prototype, 'clientHeight', 'get')
+      .mockReturnValue(100);
+    jest
+      .spyOn(HTMLElement.prototype, 'clientWidth', 'get')
+      .mockReturnValue(100);
+  });
+
+  it('should render correctly', () => {
+    const wrapper = render(
+      <DonutChart data={mockData} index={index} category={category} />,
+    );
+
+    expect(() => wrapper.unmount()).not.toThrow();
+  });
+
+  it('should render "No data" when chart data is empty', () => {
+    const { getByText } = render(<DonutChart data={[]} index='' category='' />);
+    const text = getByText(/No data/i);
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should render label', () => {
+    const LABEL = 'Label';
+    const { getByText } = render(
+      <DonutChart
+        data={mockData}
+        index={index}
+        category={category}
+        label={LABEL}
+      />,
+    );
+    const text = getByText(RegExp(LABEL, 'i'));
+
+    expect(text).toBeInTheDocument();
+  });
+});
